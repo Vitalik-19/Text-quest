733bb1d3595c7987760c3cfafd1653ceebb2082b
Vitalii Shtoiko <vetaly19k@gmail.com>
2019-11-19 22:30:51 +0200

    * add navigation hots * optimization code

--- app/src/main/java/com/example/textquest/HomeFragment.kt
+++ app/src/main/java/com/example/textquest/HomeFragment.kt
@@ -1,11 +1,11 @@
 package com.example.textquest
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
 
 
 class HomeFragment : Fragment() {
@@ -17,8 +17,8 @@ class HomeFragment : Fragment() {
     private lateinit var viewModel: HomeViewModel
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.home_fragment, container, false)
     }
