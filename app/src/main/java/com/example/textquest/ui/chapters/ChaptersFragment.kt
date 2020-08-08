91277013107bff359e1413e07a4f91565028f31c
Vitalii Shtoiko <vetaly19k@gmail.com>
2020-08-08 17:33:31 +0300

    * Updated icon app * added entity linking * fixed recycler view chapterFragment

--- app/src/main/java/com/example/textquest/ui/chapters/ChaptersFragment.kt
+++ app/src/main/java/com/example/textquest/ui/chapters/ChaptersFragment.kt
@@ -13,6 +13,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.textquest.R
 import com.example.textquest.database.AppDatabase
 import com.example.textquest.databinding.ChaptersFragmentBinding
+import com.example.textquest.ui.informationPersonage.InformationPersonageFragmentArgs
 
 
 class ChaptersFragment : Fragment() {
@@ -22,12 +23,14 @@ class ChaptersFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
+        val arguments = InformationPersonageFragmentArgs.fromBundle(requireArguments())
         val application = requireNotNull(this.activity).application
         val dataSource = AppDatabase.getInstance(application).appDatabaseDao
         val viewModelFactory = ChaptersViewModelFactory(dataSource, application)
         val viewModel = ViewModelProvider(this, viewModelFactory).get(ChaptersViewModel::class.java)
         binding = DataBindingUtil.inflate(inflater, R.layout.chapters_fragment, container, false)
 
+        viewModel.initializeCreateChapters(arguments.personageKey)
         adapter = ChaptersAdapter(ChapterListener { chapterId ->
             viewModel.onChapterClicked(chapterId)
         })
--- app/src/main/java/com/example/textquest/ui/chapters/ChaptersViewModel.kt
+++ app/src/main/java/com/example/textquest/ui/chapters/ChaptersViewModel.kt
@@ -5,10 +5,19 @@ import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.example.textquest.database.AppDatabaseDao
+import com.example.textquest.database.Chapter
+import com.example.textquest.database.PersonageWithChapters
+import kotlinx.coroutines.*
 
 class ChaptersViewModel(val database: AppDatabaseDao, application: Application) : AndroidViewModel(application) {
 
-    val chapters = database.getChapters()
+    private var viewModelJob = Job()
+
+    private val uiScope = CoroutineScope(Dispatchers.Main + viewModelJob)
+
+    private val _chapters = MutableLiveData<List<Chapter>>()
+    val chapters: LiveData<List<Chapter>>
+        get() = _chapters
 
     private val _navigationToGamePlay = MutableLiveData<Long>()
     val navigationToGamePlay: LiveData<Long>
@@ -18,7 +27,27 @@ class ChaptersViewModel(val database: AppDatabaseDao, application: Application) 
         _navigationToGamePlay.value = chapterId
     }
 
+    fun initializeCreateChapters(key: Long) {
+        uiScope.launch {
+            getCreateChaptersFromDatabase(key)?.chapters.let {
+                _chapters.value = it
+            }
+        }
+    }
+
     fun onGamePlayNavigated() {
         _navigationToGamePlay.value = null
+    }
+
+    private suspend fun getCreateChaptersFromDatabase(key: Long): PersonageWithChapters? {
+        return withContext(Dispatchers.IO) {
+            val chapters = database.getPersonageWithChapters(key)
+            chapters
+        }
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        viewModelJob.cancel()
     }
 }
